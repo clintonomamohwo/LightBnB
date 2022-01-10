aa9720860fc8fe48a23c507b9509027198c053b6
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2022-01-09 21:44:27 -0500

    LighthouseBnB Reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -66,8 +66,8 @@ const addUser =  function(user) {
     VALUES($1 ,$2, $3)
     RETURNING *;
   `, [user.name, user.email, user.password])
-  .then(res => res.rows)
-  .catch(err => console.error('query error', err.stack));
+    .then(res => res.rows)
+    .catch(err => console.error('query error', err.stack));
 };
 exports.addUser = addUser;
 
@@ -78,8 +78,28 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+// eslint-disable-next-line camelcase
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  const queryString = `
+  SELECT properties.*, reservations.*, avg(rating) as average_rating
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON properties.id = property_reviews.property_id 
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `;
+  // eslint-disable-next-line camelcase
+  const values = [guest_id, limit];
+  return pool.query(queryString, values)
+    .then(res => {
+      return res.rows;
+    })
+    .catch(err => {
+      return console.log('query error:', err);
+    });
 };
 exports.getAllReservations = getAllReservations;
 
